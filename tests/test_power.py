7dfa40dc56429ac37f79c36f867f84b369f0bae3
steven-murray <steven.murray@curtin.edu.au>
2017-02-20 16:26:26 +0800

    fixed tests for new API

--- tests/test_power.py
+++ tests/test_power.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy.fft import fftshift,fftn
 import os
 import inspect
 import sys
@@ -17,8 +16,8 @@ from powerbox import PowerBox, get_power
 def test_power1d():
     p = [0]*40
     for i in range(40):
-        pb = PowerBox(8001, dim=1, pk=lambda k: 1.0*k ** -2., boxlength=1.0, angular_freq=True, )
-        p[i], k = get_power(pb.delta_x, pb.boxlength)
+        pb = PowerBox(8001, dim=1, pk=lambda k: 1.0*k ** -2., boxlength=1.0, a=0,b=1 )
+        p[i], k = get_power(pb.delta_x, pb.boxlength,a=0,b=1)
 
     print np.mean(np.array(p),axis=0)/(1.0*k**-2.)
     assert np.allclose(np.mean(np.array(p),axis=0)[2000:],1.0*k[2000:]**-2.,rtol=2)
@@ -26,8 +25,8 @@ def test_power1d():
 def test_power1d_n3():
     p = [0]*40
     for i in range(40):
-        pb = PowerBox(8001, dim=1, pk=lambda k: 1.0*k ** -3., boxlength=1.0, angular_freq=True, )
-        p[i], k = get_power(pb.delta_x, pb.boxlength)
+        pb = PowerBox(8001, dim=1, pk=lambda k: 1.0*k ** -3., boxlength=1.0, b=1 )
+        p[i], k = get_power(pb.delta_x, pb.boxlength,b=1)
 
     print np.mean(np.array(p), axis=0)/(1.0*k ** -2.)
     assert np.allclose(np.mean(np.array(p), axis=0)[2000:], 1.0*k[2000:] ** -2., rtol=2)
@@ -36,8 +35,8 @@ def test_power1d_n3():
 def test_power1d_bigL():
     p = [0]*40
     for i in range(40):
-        pb = PowerBox(8001, dim=1, pk=lambda k: 1.0*k ** -3., boxlength=10.0, angular_freq=True, )
-        p[i], k = get_power(pb.delta_x, pb.boxlength)
+        pb = PowerBox(8001, dim=1, pk=lambda k: 1.0*k ** -3., boxlength=10.0, b=1 )
+        p[i], k = get_power(pb.delta_x, pb.boxlength,b=1)
 
     print np.mean(np.array(p), axis=0)/(1.0*k ** -2.)
     assert np.allclose(np.mean(np.array(p), axis=0)[2000:], 1.0*k[2000:] ** -2., rtol=2)
@@ -46,7 +45,7 @@ def test_power1d_bigL():
 def test_power1d_ordinary_freq():
     p = [0]*40
     for i in range(40):
-        pb = PowerBox(8001, dim=1, pk=lambda k: 1.0*k ** -3., boxlength=1.0, angular_freq=False, )
+        pb = PowerBox(8001, dim=1, pk=lambda k: 1.0*k ** -3., boxlength=1.0 )
         p[i], k = get_power(pb.delta_x, pb.boxlength)
 
     print np.mean(np.array(p), axis=0)/(1.0*k ** -2.)
@@ -57,8 +56,8 @@ def test_power1d_ordinary_freq():
 def test_power1d_halfN():
     p = [0]*40
     for i in range(40):
-        pb = PowerBox(4001, dim=1, pk=lambda k: 1.0*k ** -3., boxlength=1.0, angular_freq=True, )
-        p[i], k = get_power(pb.delta_x, pb.boxlength)
+        pb = PowerBox(4001, dim=1, pk=lambda k: 1.0*k ** -3., boxlength=1.0, b=1 )
+        p[i], k = get_power(pb.delta_x, pb.boxlength,b=1)
 
     print np.mean(np.array(p), axis=0)/(1.0*k ** -2.)
     assert np.allclose(np.mean(np.array(p), axis=0)[1000:], 1.0*k[1000:] ** -2., rtol=2)
@@ -67,16 +66,16 @@ def test_power1d_halfN():
 def test_power2d():
     p = [0]*5
     for i in range(5):
-        pb = PowerBox(200, dim=2, pk=lambda k: 1.0*k ** -2., boxlength=1.0, angular_freq=True)
-        p[i], k = get_power(pb.delta_x, pb.boxlength)
+        pb = PowerBox(200, dim=2, pk=lambda k: 1.0*k ** -2., boxlength=1.0, b=1)
+        p[i], k = get_power(pb.delta_x, pb.boxlength,b=1)
 
     print np.mean(np.array(p),axis=0)/(1.0*k**-2.)
     assert np.allclose(np.mean(np.array(p),axis=0)[100:],1.0*k[100:]**-2.,rtol=2)
 
 
 def test_power3d():
-    pb = PowerBox(50, dim=2, pk=lambda k: 1.0*k ** -2., boxlength=1.0, angular_freq=True)
-    p, k = get_power(pb.delta_x, pb.boxlength)
+    pb = PowerBox(50, dim=2, pk=lambda k: 1.0*k ** -2., boxlength=1.0, b=1)
+    p, k = get_power(pb.delta_x, pb.boxlength,b=1)
 
     print p/(1.0*k**-2.)
     assert np.allclose(p,1.0*k**-2.,rtol=2)
