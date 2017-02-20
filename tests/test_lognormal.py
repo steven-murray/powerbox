ee0a57101af084746f8343865f366382e2c0b38a
steven-murray <steven.murray@curtin.edu.au>
2017-02-20 16:26:41 +0800

    added lognormal test

---
+++ tests/test_lognormal.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+import inspect
+import sys
+
+LOCATION = "/".join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split("/")[:-1])
+sys.path.insert(0, LOCATION)
+
+from powerbox import LogNormalPowerBox,PowerBox, get_power
+
+def test_ln_vs_straight():
+    # Set up two boxes with exactly the same parameters
+    pb = PowerBox(128,lambda u : 100.*u**-2., dim=3,seed=1234,boxlength=100.)
+    ln_pb = LogNormalPowerBox(128,lambda u : 100.*u**-2., dim=3,seed=1234,boxlength=100.)
+
+
+    pk,bins = get_power(pb.delta_x,pb.boxlength)
+    ln_pk, ln_bins = get_power(ln_pb.delta_x, pb.boxlength)
+
+    pk = pk[1:-1]
+    ln_pk = ln_pk[1:-1]
+    print np.mean(np.abs((pk-ln_pk)/pk)), np.abs((pk-ln_pk)/pk)
+    assert np.mean(np.abs((pk-ln_pk)/pk)) < 2e-1   # 10% agreement
