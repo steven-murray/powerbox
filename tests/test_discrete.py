25542ec2c95e22008f217ffebe883d33e1f94400
steven-murray <steven.murray@curtin.edu.au>
2017-02-23 12:16:57 +0800

    added some tests for discrete samples

---
+++ tests/test_discrete.py
@@ -0,0 +1,33 @@
+import numpy as np
+import os
+import inspect
+import sys
+
+LOCATION = "/".join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split("/")[:-1])
+sys.path.insert(0, LOCATION)
+
+from powerbox import PowerBox, LogNormalPowerBox, get_power
+
+
+def test_discrete_power_gaussian():
+    pb = PowerBox(N=512,dim=2,boxlength=100., pk = lambda u : 0.1*u**-1.5, ensure_physical=True)
+
+    sample = pb.create_discrete_sample(nbar=1000.)
+    power, bins = get_power(sample,pb.boxlength,N=pb.N)
+
+
+    res =  np.mean(np.abs(power[50:-50] / (0.1*bins[50:-50]**-1.5) -1 ) )
+
+    print res
+    assert res < 1e-1
+
+def test_discrete_power_lognormal():
+    pb = LogNormalPowerBox(N=512, dim=2, boxlength=100., pk=lambda u: 0.1*u ** -1.5, ensure_physical=True)
+
+    sample = pb.create_discrete_sample(nbar=1000.)
+    power, bins = get_power(sample, pb.boxlength, N=pb.N)
+
+    res = np.mean(np.abs(power[50:-50]/(0.1*bins[50:-50] ** -1.5) - 1))
+
+    print res
+    assert res < 1e-1
